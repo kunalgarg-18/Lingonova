40988b9fa1f3ececbfeb31e23d4e728e71156748
Kunal Garg <imgargk18@gmail.com>
2024-07-12 21:53:01 +0530

    Customized button library

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import { Button } from "@/components/ui/button";
 export default function Home() {
   return(
     <>
-      <Button size={"lg"} variant={"destructive"}>
+      <Button size={"lg"} variant={"destructive "}>
         Click Me
       </Button>
     </>
