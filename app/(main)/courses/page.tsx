6b49668ac9b25cfc43d5c22a1c745ea64873b4ca
Kunal Garg <imgargk18@gmail.com>
2024-07-15 16:45:23 +0530

    Seeding of database done

--- app/(main)/courses/page.tsx
+++ app/(main)/courses/page.tsx
@@ -1,10 +1,18 @@
-import { getCourses } from "@/db/queries";
+import { getCourses, getUserProgress } from "@/db/queries";
 import { courses } from "@/db/schema";
 import { List } from "./list";
 
 const CoursesPage = async () => {
 
-    const courses = await getCourses();
+    const coursesData = getCourses();
+    const userProgressData = getUserProgress();
+
+    const[courses,
+        userProgress
+    ] = await Promise.all([
+            coursesData,
+            userProgressData,
+    ]);
 
     return(
         <div className="h-full max-w-[912px] px-2 mx-auto">
@@ -13,7 +21,7 @@ const CoursesPage = async () => {
             </h1>
             <List
             courses = {courses}
-            activeCourseId = {1}
+            activeCourseId = {userProgress?.activeCourseId}
             />
         </div>
     );
